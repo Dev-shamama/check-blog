147cc64950febbe2a88d45b8d8cf9a1941c53889
shamamabinshakil <dev.shamama@gmail.com>
2023-10-18 21:27:36 +0500

    initial commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import React from "react";
 const Index = async () => {
   const res = await fetch("http://localhost:3000/api/request");
   const result = await res.json();
+  // console.log(result);
   return (
     <>
       <h1>Data Render</h1>
       {
-        result.data.map((item:any) => (
+        result?.data.map((item:any) => (
           <li key={item._id}>{item.color_name}</li>
         ))
       }
