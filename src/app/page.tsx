68ea61b390454ae9a83895bc00686432d7aaa8f5
shamamabinshakil <dev.shamama@gmail.com>
2023-10-18 21:52:08 +0500

    initial commit4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-const Page = async () => {
-  const res = await fetch("http://localhost:3000/api/request");
-  const result = await res.json();
+const Page = () => {
+  const [data, setData] = useState(null);
+  const getData = async () => {
+    const res = await fetch("http://localhost:3000/api/request");
+    const result = await res.json();
+    setData(result);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   // console.log(result);
   return (
     <>
       <h1>Data Render</h1>
-      {result?.data.map((item: any) => (
+      {data && data?.data.map((item: any) => (
         <li key={item._id}>{item.color_name}</li>
       ))}
     </>
